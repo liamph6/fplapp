16e4933e55343ddb713647dd2ca6019673301bfe
liamph6 <126898938+liamph6@users.noreply.github.com>
2025-08-16 19:52:34 +0100

    Create api_requests.py

---
+++ utils/api_requests.py
@@ -0,0 +1,34 @@
+
+import streamlit as st
+import requests
+
+@st.cache_data(ttl=3600)
+def get_league_standings(league_id):
+    url = f"https://fantasy.premierleague.com/api/leagues-classic/{league_id}/standings/"
+    res = requests.get(url)
+    return res.json() if res.status_code == 200 else None
+
+@st.cache_data(ttl=3600)
+def get_player_summary(entry_id):
+    url = f"https://fantasy.premierleague.com/api/entry/{entry_id}/"
+    res = requests.get(url)
+    return res.json() if res.status_code == 200 else None
+
+@st.cache_data(ttl=3600)
+def get_gameweek_points(entry_id, gw):
+    url = f"https://fantasy.premierleague.com/api/entry/{entry_id}/event/{gw}/picks/"
+    res = requests.get(url)
+    return res.json() if res.status_code == 200 else None
+
+@st.cache_data(ttl=3600)
+def get_current_gameweek():
+    response = requests.get("https://fantasy.premierleague.com/api/bootstrap-static/")
+    data = response.json()
+    current_gw = next((event['id'] for event in data['events'] if event['is_current']), None)
+    return current_gw
+
+@st.cache_data(ttl=3600)
+def get_manager_history(entry_id):
+    url = f"https://fantasy.premierleague.com/api/entry/{entry_id}/history/"
+    response = requests.get(url)
+    return response.json()
